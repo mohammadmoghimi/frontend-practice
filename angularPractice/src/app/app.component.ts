58ee272cf134046af69d7d9db31f2a90df267928
mohammadmoghimi <mmoghimi2003@gmail.com>
2024-04-25 17:38:09 +0430

    add home

--- angularPractice/src/app/app.component.ts
+++ angularPractice/src/app/app.component.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from '../toolbar/toolbar.component';
 import { CardComponent } from '../card/card.component';
-
+import { HomeComponent } from './home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , ToolbarComponent , CardComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet , ToolbarComponent , CardComponent , HomeComponent],
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+`,
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angularPractice';
+  title = 'homes';
 }
